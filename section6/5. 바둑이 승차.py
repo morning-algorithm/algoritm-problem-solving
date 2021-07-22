a9575f822a1509dd41fae41a702153d4278557e2
Sosohy <34918049+Sosohy@users.noreply.github.com>
2021-07-23 00:35:50 +0900

    [ADD] 6-5 (DFS이용)

--- section6/5. 바둑이 승차.py
+++ section6/5. 바둑이 승차.py
@@ -1,11 +1,32 @@
 import sys
 #sys.stdin=open("input.txt", "r")
 
+def DFS(x, sum):
+    global max
+
+    if sum > c:
+        return
+    
+    if x == n:
+        if sum <= c and sum > max:
+            max = sum
+    else:
+        use[x] = 1 #숫자 사용
+        DFS(x+1, sum + w[x])
+
+        use[x] = 0 #숫자 사용X
+        DFS(x+1, sum)
+
+max = 0       
 c, n = map(int, input().split())
+use = [0]*n
 w = []
 
 for i in range(n):
     w.append(int(input()))
+
+DFS(0, 0)
+print(max)
 
 '''
 wSum = 0;
@@ -16,4 +37,4 @@ for i in range(n):
 
 print(wSum)
 
-'' 
+'''
