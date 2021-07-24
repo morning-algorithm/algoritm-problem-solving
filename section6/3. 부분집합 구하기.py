07c70bd3741d921a68be5c78b1fd72e3bdd87c02
Lee Jiwon <jw010208@gmail.com>
2021-07-24 18:34:33 +0900

    [ADD]6-3

--- section6/3. 부분집합 구하기.py
+++ section6/3. 부분집합 구하기.py
@@ -1,19 +1,19 @@
 import sys
-#sys.stdin = open("input.txt", "r")
+#sys.stdin=open("input.txt", "r")
 
-def DFS(x):
-    if x==n+1:
-        for i in range(n):
-            if ch[i]==1:
-                print(i+1, end=" ")
+def DFS(i):
+    if i == n:
+        for j in range(n):
+            if res[j]==1:
+                print(j+1, end=" ")
         print()
     else:
-        ch[x-1]=1
-        DFS(x+1)
-        ch[x-1]=0
-        DFS(x+1)
+        res[i]=1 # O - 부분 집합 포함
+        DFS(i+1)
+        res[i]=0 # X - 부분 집합 제외
+        DFS(i+1)
 
 if __name__=="__main__":
-    n=int(input())
-    ch=[0]*n
-    DFS(1)
+    n= int(input())
+    res=[0]*n # 부분 집합의 숫자 0, 1로 여부 표시
+    DFS(0)
